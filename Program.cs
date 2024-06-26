9cdb4d1d36c04aea744b5bbf2575886716a1f32c
Yago Crystian <103502519+YagoCrystian@users.noreply.github.com>
2024-06-26 16:34:39 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Reflection.PortableExecutable;
+
+
+namespace StopWatch // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("S = Segundos --> 10 = 10s");
+            Console.WriteLine("M = Minutos --> 1m = 60s");
+            Console.WriteLine("0 = Sair");
+            Console.WriteLine("Quanto tempo deseja contar? ");
+
+            string data = Console.ReadLine().ToLower();
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
+
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+            int multiplier = 1;
+
+            if (type == 'm')
+                multiplier = 60;
+
+            if (time == 0)
+                System.Environment.Exit(0);
+
+            PreStart(time * multiplier);
+
+
+        }
+
+        static void PreStart(int time)
+        {
+            Console.Clear();
+            Console.WriteLine("Ready...");
+            Thread.Sleep(1000);
+            Console.Clear();
+
+            Console.WriteLine("Set...");
+            Thread.Sleep(1000);
+            Console.Clear();
+
+            Console.WriteLine("GO!");
+            Thread.Sleep(1000);
+            Console.Clear();
+
+            Start(time);
+
+        }
+        static void Start(int time)
+        {
+            int currentTime = 0;
+
+            while (currentTime != time)
+            {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
+            }
+
+            Console.Clear();
+            Console.WriteLine("StopWatch Finalizado...");
+
+            Thread.Sleep(2000);
+
+            Menu();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,51 @@
+# Simple Stopwatch Console App
+
+![GitHub repo size](https://img.shields.io/github/repo-size/YagoCrystian/StopWatch)
+
+
+
+## Descrição
+
+Um simples aplicativo de cronômetro em console escrito em C#. Este projeto permite aos usuários configurar um cronômetro regressivo em segundos ou minutos e exibir a contagem regressiva no console.
+
+## Funcionalidades
+
+- Configuração de cronômetro em segundos (S) e minutos (M).
+- Interface de usuário simples baseada em console.
+- Reinício automático do cronômetro após completar a contagem regressiva.
+
+## Como Usar
+
+1. **Clone o repositório:**
+
+   ```bash
+   git clone https://github.com/YagoCrystian/StopWatch.git
+   
+2. **Acesse seu file:**
+   ```bash
+    cd StopWatch
+
+3. **Compile e rode:**
+    ```bash
+    dotnet run
+
+4. **Menu Principal:**
+> Ao iniciar o programa, um menu será exibido com as seguintes opções:
+
+    S = Segundos: Digite 10s para configurar um cronômetro de 10 segundos.
+    M = Minutos: Digite 1m para configurar um cronômetro de 1 minuto.
+    0 = Sair: Encerra o programa.
+    Exemplo de Uso:
+
+    Para iniciar um cronômetro de 1 minuto, digite 1m e pressione Enter.
+    O cronômetro começará a contagem regressiva até zero e reiniciará automaticamente.
+    Para sair do programa, digite 0 e pressione Enter.
+
+
+## Estrutura do Código
+Program.cs: Contém a lógica principal do programa, incluindo a navegação do menu e as funções para configurar e executar o cronômetro.
+## Contribuição
+Contribuições são bem-vindas! Sinta-se à vontade para abrir uma issue ou enviar um pull request.
+
+## Licença
+Este projeto está licenciado sob a Licença MIT - veja o arquivo LICENSE para mais detalhes.
